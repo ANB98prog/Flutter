23a0b28e47675185fbc98bd775f0cd16dd4c4370
Alex Newman <bakaev_a1998@mail.ru>
2023-07-06 21:51:28 +0600

    Сделал более продвинутую версию

---
+++ namer/lib/myapp.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:namer/pages/myhomepage.dart';
+import 'package:provider/provider.dart';
+
+import 'myappstate.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
---
+++ namer/lib/widgets/history_list_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:namer/myappstate.dart';
+import 'package:provider/provider.dart';
+
+class HistoryListView extends StatefulWidget {
+  const HistoryListView({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HistoryListViewState();
+}
+
+class _HistoryListViewState extends State<HistoryListView> {
+  // Нужен чтобы [MyAppState] смог сказать [AnimatedList]
+  // анимировать новые элементы
+  final _key = GlobalKey();
+
+  static const Gradient _maskGradient = LinearGradient(
+      colors: [Colors.transparent, Colors.black],
+      stops: [0.0, 0.5],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter);
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = context.watch<MyAppState>();
+    appState.historyListKey = _key;
+
+    return ShaderMask(
+      shaderCallback: (bounds) => _maskGradient.createShader(bounds),
+      blendMode: BlendMode.dstIn,
+      child: AnimatedList(
+        key: _key,
+        reverse: true,
+        padding: EdgeInsets.only(top: 100),
+        initialItemCount: appState.history.length,
+        itemBuilder: (context, index, animation) {
+          final pair = appState.history[index];
+          return SizeTransition(
+            sizeFactor: animation,
+            child: Center(
+              child: TextButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite(pair);
+                  },
+                  icon: appState.favorites.contains(pair)
+                      ? Icon(Icons.favorite, size: 12)
+                      : SizedBox(),
+                  label: Text(
+                    pair.asLowerCase,
+                    semanticsLabel: pair.asPascalCase,
+                  )),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
